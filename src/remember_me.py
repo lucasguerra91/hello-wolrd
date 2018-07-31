f02c73b2e413b6af1463a155512e3b338c99cf3d
Lucas <guerra986@gmail.com>
2018-07-30 23:27:13 -0300

    employee class

--- src/remember_me.py
+++ src/remember_me.py
@@ -2,11 +2,11 @@ import json
 
 
 def get_stored_username():
-<<<<<<< HEAD
+
     """Get stored username if available."""
-=======
+
     """Get stored username if available"""
->>>>>>> 94fea0cf389c4695eb93b8b8c53796fec5438f9b
+
     filename = 'username.json'
     try:
         with open(filename) as f_obj:
@@ -18,18 +18,13 @@ def get_stored_username():
 
 
 def get_new_username():
-<<<<<<< HEAD
-    """Prompt for a new username."""
-    username = input("What is your name? ")
-=======
     """Prompt for a new user name"""
     username = input('Enter the new name :  ')
->>>>>>> 94fea0cf389c4695eb93b8b8c53796fec5438f9b
+
     filename = 'username.json'
     with open(filename, 'w') as f_obj:
         json.dump(username, f_obj)
     return username
-<<<<<<< HEAD
 
 
 def greet_user():
@@ -42,7 +37,7 @@ def greet_user():
         print("We'll remember you when you come back, " + username + "!")
 
 
-=======
+
 
 def greet_user():
     """Greet the user by name."""
@@ -55,7 +50,6 @@ def greet_user():
         print("We'll remember you when you come back, " + username + "!")
 
 
->>>>>>> 94fea0cf389c4695eb93b8b8c53796fec5438f9b
 greet_user()
 
 
