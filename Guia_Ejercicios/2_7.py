96b1feab9dfcf44b0a7b695c5d54f35597f11049
Lucas Guerra <guerra986@gmail.com>
2019-03-23 14:40:20 -0300

    Update 2_7.py

--- Guia_Ejercicios/2_7.py
+++ Guia_Ejercicios/2_7.py
@@ -7,6 +7,6 @@ def imprimir_domino():
     for i in range(0, 7):
         for j in range(0, i+1):
             print('.' * j,  '.' * i, sep='|', end='\n')
-
+        print('\n')
 
 imprimir_domino()
