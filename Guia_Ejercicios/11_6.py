68251349ff8640d1ee4cd3176b097a593510cc13
Lucas <guerra986@gmail.com>
2019-05-23 02:30:15 -0300

    Ejs 11.7 8 y 9

--- Guia_Ejercicios/11_6.py
+++ Guia_Ejercicios/11_6.py
@@ -17,9 +17,7 @@ class ListaEnlazada:
     def __len__(self):
         return self.len
 
-    # Ejercicio 11.6
     def __str__(self):
-        """ Genera una salida legible de lo que contiene la lista, similar a las listas de Python"""
         lista = []
         actual = self.prim
         while actual:
@@ -165,80 +163,6 @@ class ListaEnlazada:
             self.prim = self.ultimo = nuevo
         self.len += 1
 
-    # Ej 11.7
-    def extend(self, otra):
-        """ Se extiende la lista con otra que se recibe como parametro """
-
-        if not self.no_vacia() or not otra.no_vacia():
-            raise ValueError("Una de las listas esta vacia")
-
-        self.ultimo.prox = otra.prim
-        self.ultimo = otra.ultimo
-        self.len += otra.len
-
-    # Ej 11.8
-    def remover_todos(self, elemento):
-        """ Remueve todas las apariciones del elemento en la lista y devuelve la cantidad removida.
-        Si esta vacia levanta error, si el elemento no esta levanta error """
-
-        if self.no_vacia():
-            try:
-                borrados = 0
-                ant = None
-                act = self.prim
-
-                while act:
-                    if act.dato == elemento:
-
-                        # evalua el primero
-                        if act == self.prim:
-                            self.prim = self.prim.prox
-
-                        # en caso de que sea el último
-                        elif act == self.ultimo:
-                            self.ultimo = ant
-                            ant.prox = None
-
-                        # en caso de que sea un don nadie
-                        else:
-                            ant.prox = act.prox
-
-                        borrados += 1
-                    ant = act
-                    act = ant.prox
-
-                self.len -= borrados
-                return borrados
-            except:
-                return 0
-        else:
-            raise ValueError("La lista está vacía.")
-
-    # Ej 11.9
-    def duplicar_elemento(self, elemento):
-        """Recibe un elemento y duplica todas sus apariciones dentro de la lista """
-        if self.no_vacia():
-            act = self.prim
-            while act:
-                if act.dato == elemento:
-                    # Ojo con crearlo afuera del while, puede ser mas de uno
-                    nuevo = n.Nodo(elemento)
-                    if act == self.ultimo:
-                        act.prox = nuevo
-                        nuevo.prox = None
-                        self.ultimo = nuevo
-                    else:
-                        nuevo.prox = act.prox
-                        act.prox = nuevo
-
-                    self.len += 1
-                    # Si lo agrego no lo evalúo en la sgte vuelta
-                    act = act.prox.prox
-                else:
-                    act = act.prox
-        else:
-            raise ValueError("Lista vacía.")
-
 
 li = ListaEnlazada()
 
@@ -267,50 +191,10 @@ li.pop()
 li.pop(2)
 
 print(li)
-# for valor in li:
-#     print(valor)
-#
-# print('\n')
-# print(f"Primero : {li.prim}")
-# print(f"Ultimo: {li.ultimo}")
-# print(f"Longitud: {li.len}")
+for valor in li:
+    print(valor)
 
-li2 = ListaEnlazada()
-li2.insertar_al_final('Hola4')
-li2.insertar_al_final('Hola5')
-li2.insertar_al_final('Hola6')
-
-print(li2)
-li.extend(li2)
-print(li)
-print(f"\nPrimero : {li.prim}")
-print(f"Ultimo: {li.ultimo}")
-print(f"Longitud: {li.len}")
-
-# li3 = ListaEnlazada()
-# li.extend(li3)
-
-li.insertar_al_final('test')
-print(li)
-# print(f"Se removieron {li.remover_todos('test')} apariciones de 'test'")
-print(f"\nSe removieron {li.remover_todos('Hola')} apariciones de 'Hola'")
-print(li)
+print('\n')
 print(f"Primero : {li.prim}")
 print(f"Ultimo: {li.ultimo}")
 print(f"Longitud: {li.len}")
-
-li3 = ListaEnlazada()
-li3.insertar_al_final(8)
-li3.insertar_al_final(5)
-li3.insertar_al_final(8)
-li3.insertar_al_final(8)
-li3.insertar_al_final(1)
-li3.insertar_al_final(8)
-
-print(li3)
-li3.duplicar_elemento(8)
-
-print(li3)
-print(f"Primero : {li3.prim}")
-print(f"Ultimo: {li3.ultimo}")
-print(f"Longitud: {li3.len}")
