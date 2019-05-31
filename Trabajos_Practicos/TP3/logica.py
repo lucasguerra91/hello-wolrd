9a0381541bddba410a4f32d1cfb655170c801139
Lucas Guerra <guerra986@gmail.com>
2019-05-31 20:25:53 -0300

    terminando tp3

---
+++ Trabajos_Practicos/TP3/logica.py
@@ -0,0 +1,84 @@
+import tortuga as t
+import pila as p
+
+
+def cargar_archivo(archivo):
+    """
+    Recibe el archivo del fractal, devuelve el angulo, axioma y lista de traducciones
+    """
+
+    with open(archivo, 'r')as f:
+        angulo = float(f.readline())
+        axioma = f.readline().rstrip('\n')
+        traducciones = {}
+
+        for linea in f:
+            clave, valor = linea.split()
+            traducciones[clave] = traducciones.get(clave, valor)
+
+    return angulo, axioma, traducciones
+
+
+def traducir(axioma, traducciones, cantidad):
+    """
+
+    :param axioma:  cadena que define el estado inicial
+    :param traducciones: lista de traducciones predecesor - sucesor
+    :return: secuencia traducida
+    """
+    secuencia = ''
+    while cantidad > 0:
+        for c in axioma:
+            if c in traducciones:
+                secuencia += traducciones.get(c)
+            else:
+                secuencia += c
+        return traducir(secuencia, traducciones, cantidad - 1)
+    return axioma
+
+
+def crear_svg(destino, secuencia, angulo):
+    """
+
+    :param secuencia:
+    :param angulo:
+    :return:
+    """
+    unidad = 1
+    with open(destino, 'w') as f:
+
+        f.write('<svg viewBox="-400 -300 800 600" xmlns="http://www.w3.org/2000/svg">\n')
+
+        tortuguero = p.Pila()
+        tortuguero.apilar(t.Tortuga(0, 0, 270, True))
+
+        dibujar(secuencia, tortuguero, unidad, angulo, f)
+
+        f.write('</svg>\n')
+
+
+def dibujar(secuencia, tortuguero, unidad, angulo, f):
+
+    for c in secuencia:
+        if c == 'F' or c == 'G':
+            tortuguero.ver_tope().avanzar(unidad, f)
+
+        if c == 'f' or c == 'g':
+            tortuguero.ver_tope().pluma_arriba()
+            tortuguero.ver_tope().avanzar(unidad)
+            tortuguero.ver_tope().pluma_abajo()
+
+        if c == '+':
+            tortuguero.ver_tope().girar_derecha(angulo)
+
+        if c == '-':
+            tortuguero.ver_tope().girar_izquierda(angulo)
+
+        if c == '|':
+            tortuguero.ver_tope().girar_derecha(180)
+
+        if c == '[':
+            tortuguero.apilar(tortuguero.ver_tope().clonar())
+
+        if c == ']':
+            tortuguero.desapilar()
--- Trabajos_Practicos/TP3/sistemasL.py
+++ Trabajos_Practicos/TP3/sistemasL.py
@@ -1,83 +1,22 @@
-import turtle
 import os
+import logica as l
 
 
-def cargar_archivo(archivo):
-    """
-    Recibe el archivo del fractal, devuelve el angulo, axioma y lista de traducciones
-    """
-
-    with open(archivo, 'r')as f:
-        angulo = int(f.readline())
-        axioma = f.readline().rstrip('\n')
-        traducciones = {}
-
-        for linea in f:
-            clave, valor = linea.split()
-            traducciones[clave] = traducciones.get(clave, valor)
-
-    return angulo, axioma, traducciones
-
-
-def traducir(axioma, traducciones, cantidad):
-    """
-
-    :param axioma:  cadena que define el estado inicial
-    :param traducciones: lista de traducciones predecesor - sucesor
-    :return: secuencia traducida
-    """
-    secuencia = ''
-    while cantidad > 0:
-        for c in axioma:
-            if c in traducciones:
-                secuencia += traducciones.get(c)
-            else:
-                secuencia += c
-        return traducir(secuencia, traducciones, cantidad - 1)
-    return axioma
-
-
-def dibujar_con_tortuga(secuencia, angulo):
-    """
-
-    :param secuencia:
-    :param angulo:
-    :return:
-    """
-
-    tortuga = turtle.Turtle()
-    for c in secuencia:
-
-        if c == 'F' or c == 'G':
-            tortuga.fd(1)
-
-        if c == 'f' or c == 'g':
-            tortuga.up()
-            tortuga.fd(1)
-            tortuga.down()
-
-        if c == '+':
-            tortuga.rt(angulo)
-
-        if c == '-':
-            tortuga.lt(angulo)
-
-        if c == '|':
-            tortuga.rt(180)
-
-
-def main(archivo, cantidad):
+def main(archivo, cantidad, destino):
     """
 
     :return:
     """
-    angulo, axioma, traducciones = cargar_archivo(archivo)
-    secuencia_comandos = traducir(axioma, traducciones, cantidad)
+    angulo, axioma, traducciones = l.cargar_archivo(archivo)
+    secuencia_comandos = l.traducir(axioma, traducciones, cantidad)
+    print(secuencia_comandos)
 
-    dibujar_con_tortuga(secuencia_comandos, angulo)
+    #l.dibujar_con_tortuga(secuencia_comandos, angulo)
+    l.crear_svg(destino, secuencia_comandos, angulo)
 
 
 # ejecucion
-main('arbol1.sl', 9)
-os.system('pause')
+main('sier1.sl', 7, 'test.svg')
+
+#os.system('pause')
 
